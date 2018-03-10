80a2030518cb1f9ab25abcf2f4417d6c911f409e
Alex Tan <tan.alex219@gmail.com>
2018-03-10 18:26:09 -0500

    added more files to .gitignore

--- parser.py
+++ parser.py
@@ -1,13 +1,15 @@
 import urllib.request
 import constants
 
-def urlBuilder (list) -> str:
+
+def urlBuilder(list) -> str:
     url = constants.BASEURL
-    separator = '&' 
+    separator = '&'
     restOfURL = separator.join(list)
     url = url + restOfURL
     return url
-    
+
+
 with open('inputs/symbols', 'r') as f:
     read_data = f.read()
 f.closed
@@ -15,13 +17,12 @@ f.closed
 formatted_data = str(read_data).split()
 
 for item in formatted_data:
-    paramList = [constants.TIME_SERIES_DAILY_ADJUSTED,
-    'symbol=' + item,
-    constants.OUTPUTSIZE_COMPACT,
-    constants.DATATYPE_CSV,
-    constants.API_KEY]
+    paramList = [
+        constants.TIME_SERIES_DAILY_ADJUSTED, 'symbol=' + item,
+        constants.OUTPUTSIZE_COMPACT, constants.DATATYPE_CSV, constants.API_KEY
+    ]
 
     url = urlBuilder(paramList)
-    print (url)
+    print(url)
 
-    urllib.request.urlretrieve(url,'outputs/test_' + item + '.csv')
+    urllib.request.urlretrieve(url, 'outputs/test_' + item + '.csv')
