7296278c2b435618e0aa6ca8e9c4d1ca7b61ead2
Alex Tan <tan.alex219@gmail.com>
2018-03-13 21:37:49 -0400

    Refactor: changed all " to ' and added return type to util functions where applicable

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,10 +1,10 @@
-"""constants"""
+'''constants'''
 
 # fetch script constants
-API_KEY = "apikey=NHX8KHJFCBEJFJ7P"
-BASEURL = "https://www.alphavantage.co/query?"
-TIME_SERIES_DAILY_ADJUSTED = "function=TIME_SERIES_DAILY_ADJUSTED"
-DATATYPE_JSON = "datatype=json"
-DATATYPE_CSV = "datatype=csv"
-OUTPUTSIZE_COMPACT = "outputsize=compact"
-OUTPUTSIZE_FULL = "outputsize=full"
+API_KEY = 'apikey=NHX8KHJFCBEJFJ7P'
+BASEURL = 'https://www.alphavantage.co/query?'
+TIME_SERIES_DAILY_ADJUSTED = 'function=TIME_SERIES_DAILY_ADJUSTED'
+DATATYPE_JSON = 'datatype=json'
+DATATYPE_CSV = 'datatype=csv'
+OUTPUTSIZE_COMPACT = 'outputsize=compact'
+OUTPUTSIZE_FULL = 'outputsize=full'
--- scripts/csv_reader.py
+++ scripts/csv_reader.py
@@ -3,12 +3,14 @@ import csv
 import sys
 import utils
 
-def read(csv_relative_path):
+
+def read(csv_relative_path) -> list:
     '''csv_relative_path is the path to the csv file from project root'''
     csv_file_path = utils.format_path(csv_relative_path)
     with open(csv_file_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
         return list(csv_reader)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     print(read(sys.argv[1]))
