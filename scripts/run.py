39b60890feba4abba7aa359ac133954651121d72
Josh Sun <joshuasun1998@icloud.com>
2018-03-17 14:41:58 -0700

    created configs for env variables

--- scripts/run.py
+++ scripts/run.py
@@ -12,22 +12,34 @@ def main(argv):
     except getopt.GetoptError:
         print('run.py')
         sys.exit(2)
-    dev = True
+    env = 'dev'
     for opt, _ in opts:
         if opt in ('-p', '--production'):
             print('Running in production mode')
-            dev = False
-    if dev:
+            env = 'prod'
+    if env == 'dev':
         print('Running in development mode')
 
     # data pipe line:
     # fetch -> validate -> normalize -> dump into db
+    context = {
+        'dev': {
+            'input_symbols': 'input/symbols',
+            'output_raw': 'output/dev/raw',
+            'output_valid': 'output/dev/valid'
+        },
+        'prod': {
+            'input_symbols': 'input/symbols',
+            'output_raw': 'output/prod/raw',
+            'output_valid': 'output/prod/valid'
+        }
+    }
 
     # fetch
-    fetch_stock_data.fetch('input/symbols', 'output/dev/raw')
+    fetch_stock_data.fetch(context[env]['input_symbols'], context[env]['output_raw'])
 
     # validate
-    validate_stock_data.validate('output/dev/raw', 'output/dev/valid')
+    validate_stock_data.validate(context[env]['output_raw'], context[env]['output_valid'])
 
 
 if __name__ == '__main__':
