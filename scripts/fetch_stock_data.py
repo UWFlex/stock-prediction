a349a1e9597bd8556090c8af1cd02da2efe5ff6f
Josh Sun <joshuasun1998@icloud.com>
2018-03-18 19:51:30 -0700

    changed fetch script

--- scripts/fetch_stock_data.py
+++ scripts/fetch_stock_data.py
@@ -1,32 +1,32 @@
 '''fetches stock data from api'''
 import urllib.request
 import sys
+import json
+import pandas as pd
 import constants
 import utils
 
 
-def fetch(symbols_file, output_path):
-    '''fetches stock data from api, then outputs as raw csv file'''
-#    base_path = os.path.dirname(os.path.realpath('__file__'))
+def fetch(symbol, config):
+    '''fetches stock data from api, return as a pandas dataframe'''
 
-    # read from symbols file
-    with open(utils.format_path(symbols_file), 'r') as data:
-        read_data = data.read()
-        formatted_data = str(read_data).split()
+    # fetch stock data for a symbol
+    param_list = [
+        'function=' + config['function'],
+        'symbol=' + symbol,
+        'outputsize=' + config['outputsize'],
+        'datatype=' + config['datatype'],
+        'apikey=' + config['apikey']
+    ]
 
-        # fetch stock data for each symbol
-        for item in formatted_data:
-            param_list = [
-                constants.TIME_SERIES_DAILY_ADJUSTED, 'symbol=' + item,
-                constants.OUTPUTSIZE_COMPACT, constants.DATATYPE_CSV, constants.API_KEY
-            ]
+    raw_url = utils.url_builder(constants.BASEURL, param_list)
 
-            url = utils.url_builder(constants.BASEURL, param_list)
-            utils.make_dir_if_not_exists(output_path)
-            urllib.request.urlretrieve(url, utils.format_path(
-                output_path) + '/' + item + '.csv')
-            print('fetched ' + utils.format_path(output_path) + '/' + item + '.csv')
+    with urllib.request.urlopen(raw_url) as url:
+        data = json.loads(url.read().decode())
+        dataframe = pd.DataFrame(data['Time Series (Daily)'])
+        # print(dataframe)
+        return dataframe
 
 
 if __name__ == '__main__':
-    fetch(str(sys.argv[1]), str(sys.argv[2]))
+    fetch(str(sys.argv[1]), sys.argv[2])
